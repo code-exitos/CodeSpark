948d6627d1ef5188b4f6e50275b1750fe65dbd36
afloresmorales <afloresmorales96@gmail.com>
2020-07-09 13:57:23 -0600

    adding class example.

--- code-spark-04-react-hooks/src/App.js
+++ code-spark-04-react-hooks/src/App.js
@@ -2,25 +2,74 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      movies: [],
+      error: null,
+      favoriteMovie: '',
+      releaseYear: ''
+    }
+  }
+  componentDidMount() {
+    document.title = `Add your favorite movie`;
+    fetch("https://reactnative.dev/movies.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            movies: result.movies
+          });
+        },
+        (error) => {
+          this.setState({
+            error
+          });
+        }
+      )
+  }
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.favoriteMovie !== this.state.favoriteMovie){
+      document.title = `New Movie: ${this.state.favoriteMovie}`
+    }
+  }
+  handleInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  addMovieToFavoriteMoviesList = () => {
+    const movieObject = {title: this.state.favoriteMovie, releaseYear: this.state.releaseYear};
+    this.setState(state => {
+      const movies = [...state.movies, movieObject];
+      return {
+        movies,
+        favoriteMovie: '',
+        releaseYear: '',
+      };
+    });
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <code>React Class</code>
+          <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            {
+            this.state.movies.map((movie, index) => (
+              <li key={index}>{movie.title}, {movie.releaseYear}</li>
+            ))}
+          </ul>
+          <div>
+            <input name='favoriteMovie' value={this.state.favoriteMovie} onChange={this.handleInputChange} placeholder='Favorite Movie' />
+            <input name='releaseYear' value={this.state.releaseYear} onChange={this.handleInputChange} placeholder='Release Year' />
+            <button onClick={this.addMovieToFavoriteMoviesList}>Add</button>
+          </div>
+          <span>{this.state.favoriteMovie}</span>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
