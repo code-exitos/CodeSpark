16fdacb8c055b3c12f609739e8c7272913ea828c
afloresmorales <afloresmorales96@gmail.com>
2020-07-09 14:00:58 -0600

    Adding Hooks example.

--- code-spark-04-react-hooks/src/App.js
+++ code-spark-04-react-hooks/src/App.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { useFieldState } from './hooks';
+//"https://reactnative.dev/movies.json"
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [fields, handleFieldsInput] = useFieldState({
+    favoriteMovie: '',
+    releaseYear: ''
+  });
+  const {favoriteMovie, releaseYear} = fields;
+  useEffect(()=>{
+    fetch("https://reactnative.dev/movies.json")
+    .then(res => res.json())
+    .then(result => setMovies(result.movies))
+  }, [])
+
+  useEffect(()=> {
+    document.title = `New Movie: ${favoriteMovie}`
+  },[favoriteMovie])
+
+  const addFavoriteMovieToList = () => {
+    const movieObject = {title: favoriteMovie, releaseYear};
+    const newMoviesList = [...movies, movieObject];
+    setMovies(newMoviesList);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <code>React Hooks</code>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {
+            movies.map((movie, index)=> (
+            <li key={index}>{movie.title}, {movie.releaseYear}</li>
+            ))
+          }
+        </ul>
+        <div>
+          <input name='favoriteMovie' value={fields.favoriteMovie} onChange={handleFieldsInput} placeholder='Favorite movie' />
+          <input value={fields.releaseYear} name='releaseYear' onChange={handleFieldsInput} placeholder='Release Year' />
+          <button onClick={addFavoriteMovieToList}>Add</button>
+        </div>
       </header>
     </div>
   );
